a98beabcac556d51c895dda2457b0e157671cdda
jade2253 <jakob.delander2253@student.uu.se>
2022-10-07 14:13:36 +0200

    Test med tidtagning c++

--- MA4_2.py
+++ MA4_2.py
@@ -2,6 +2,8 @@
 from numba import njit
 from person import Person
 import time as t
+import matplotlib as plt
+import numpy as np
 
 def fib_py(n):
 	if n <= 1:
@@ -20,10 +22,20 @@ def main():
 	f = Person(5)
 	print(f.get())
 	f.set(7)
-	res = f.fib()
+	toTest = np.arange(30, 45)
+	resCpp = np.array
+	print(resCpp)
+	#Time for fib in c++
+	for i in range(toTest):
+		f.set(i)
+		start = t.perf_counter()
+		f.fib()
+		end = t.perf_counter()
+		resCpp = np.append(start-end)
+	print(resCpp)
+	
 	print(fib_py(7))
 	print(fib_numba(7))
-	print(res)
 	print(f.get())
 
 if __name__ == '__main__':
